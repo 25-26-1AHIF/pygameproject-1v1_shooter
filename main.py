5ea36e8877d861b7ea5bbadc0189b39321a011d9
Tizian <tizian.schwarzbauer@student.htl-rankweil.at>
2026-05-12 15:10:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pygame
 # Initialisierung
 pygame.init()
-pygame.display.set_caption("Hello pygame")
-screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("1v1 Shooter")
+screen = pygame.display.set_mode((1080, 780))
+
 # Status der Main Loop (weitermachen solange True)
 running = True
+
 # Die Main Loop (Game Loop)
 while running:
- # Jedes Ereignis (Event) durchgehen
+    # Jedes Ereignis (Event) durchgehen
  for event in pygame.event.get():
-   # Das Spiel verlassen, falls der Benutzer das Fenster schließen möchte
+    # Das Spiel verlassen, falls der Benutzer das Fenster schließen möchte
  if event.type == pygame.QUIT:
-  running = False
+    running = False
 
  # Weitere Events abfragen (z.B. Tastatureingaben)
  # Update der Spiellogik
