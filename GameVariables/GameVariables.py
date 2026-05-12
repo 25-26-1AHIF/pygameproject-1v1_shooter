387edd804a9bd0e67f19c12be99ead14ea8077ec
Liam <liam.kaunelis@student.htl-rankweil.at>
2026-05-12 19:29:44 +0200

    gleich wie davor
    Signed-off-by: Liam <liam.kaunelis@student.htl-rankweil.at>

--- GameVariables/GameVariables.py
+++ GameVariables/GameVariables.py
@@ -1,2 +1,23 @@
+import pygame
+
 class GameVariables:
     FPS = 60
+    SCREEN_HEIGHT = 600
+    SCREEN_WIDTH = 800
+
+    FONT_BIG:pygame.sysfont.Font = None
+    FONT_MIDDLE:pygame.sysfont.Font = None
+    FONT_SMALL:pygame.sysfont.Font = None
+
+    @staticmethod
+    def init():
+        pygame.init()
+        GameVariables.FONT_BIG = pygame.sysfont.SysFont(name="arial", size=55, bold = True)
+        GameVariables.FONT_MIDDLE =  pygame.sysfont.SysFont(name="arial", size=40, bold = True)
+        GameVariables.FONT_SMALL =  pygame.sysfont.SysFont(name="arial", size=14, bold = True)
+
+class GameScreens:
+    MENUE = "MENUE"
+    PLAY = "PLAY"
+    EXIT = "EXIT"
+    actual = MENUE
