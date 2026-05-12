4ee41c50d5d5d4afa6feecc8264b955a19e61725
Liam Kaunelis <liam.kaunelis@student.htl-rankweil.at>
2026-05-12 16:03:53 +0200

    main.py einrückeungen gemacht + erster screen gemacht

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import pygame
-# Initialisierung
-pygame.init()
-pygame.display.set_caption("1v1 Shooter")
-screen = pygame.display.set_mode((1080, 780))
+from GameVariables.GameVariables import GameVariables as gv
 
-# Status der Main Loop (weitermachen solange True)
-running = True
+def main_screen(screen: pygame.Surface ,clock: pygame.time.Clock, ):
+    pygame.init()
+    pygame.display.set_caption("1v1 Shooter")
 
-# Die Main Loop (Game Loop)
-while running:
-    # Jedes Ereignis (Event) durchgehen
- for event in pygame.event.get():
-    # Das Spiel verlassen, falls der Benutzer das Fenster schließen möchte
- if event.type == pygame.QUIT:
-    running = False
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        clock.tick(gv.FPS)
+        screen.fill("blue")
+        pygame.display.flip()
 
- # Weitere Events abfragen (z.B. Tastatureingaben)
- # Update der Spiellogik
- # Neu zeichnen der Grafiken
- # Das Display updaten
- pygame.display.flip()
-# PyGame sauber beenden (cleanup)
-pygame.quit()
+    pygame.quit()
+
+if __name__ == '__main__':
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    main_screen(screen, clock)
